da9e033d66bc15c2af17e639f87899798a1ab4de
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-05-20 13:40:01 +0100

    finish exercise 7

---
+++ ex07.rb
@@ -0,0 +1,26 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow' # string formatting
+puts "And everywhere that Mary went."
+puts "." * 10 # string multiplication creates repeated strings
+
+end1 = "C" # variable assignment
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print "puts" everything on the same line (i.e. there is no \n)
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# when you use print (as above)
+# there is no new line character so the
+# command prompt continues on the same line
+puts
